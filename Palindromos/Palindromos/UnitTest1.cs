3a7ce87754073c7afaa9ee59ad20a06c7b1242ca
Ingrid Nataly Garcia Nivia <ingrid.garcia@sinco.com.co>
2025-10-21 18:12:23 -0500

    🟥rojo

--- Palindromos/Palindromos/UnitTest1.cs
+++ Palindromos/Palindromos/UnitTest1.cs
@@ -27,11 +27,27 @@ public class UnitTest1
         resultado.Should().Be(true);
     }
 
+    [Fact]
+    public void Reversar_Cadena()
+    {
+        //arrange
+        var cadena = "5Oso5";
+        //act
+        var resultado = ValidarPalindromo(cadena);
+        //assert
+        resultado.Should().Be(false);
+    }
+
     private Boolean ValidarPalindromo(string cadena)
     {
         string Patron = "[!\"·$%&/()=¿¡?'_:;,|@#€*+.]";
          string nuevaCadena =Regex.Replace(cadena.ToLower(),Patron, "");  
        Console.WriteLine(nuevaCadena); 
+       char[] caracteres = nuevaCadena.ToCharArray();
+       Array.Reverse(caracteres);
+       string cadenaInvertida = new string(caracteres);
+       
+       
        if (nuevaCadena.Any(char.IsUpper))
         {
             return false;
@@ -39,6 +55,11 @@ public class UnitTest1
         if ( Regex.IsMatch(nuevaCadena, Patron))
         {
             
+            return false;
+        }
+
+        if (nuevaCadena!=cadenaInvertida)
+        {
             return false;
         }
         return true;
