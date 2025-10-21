539c0d77118746a962e37573452120ef3f0550c2
Ingrid Nataly Garcia Nivia <ingrid.garcia@sinco.com.co>
2025-10-21 16:38:01 -0500

    🟥rojo

---
+++ Palindromos/Palindromos/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using AwesomeAssertions;
+
+namespace Palindromos;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Ignorar_Mayusculas_Y_Minusculas_De_La_Cadena()
+    {
+        //arrange
+        var cadena = "Oso";
+        //act
+        var resultado = ValidarPalindromo(cadena);
+        //assert
+        resultado.Should().Be(true);
+    }
+
+    private Boolean ValidarPalindromo(string cadena)
+    {
+        throw new NotImplementedException();
+    }
+}
