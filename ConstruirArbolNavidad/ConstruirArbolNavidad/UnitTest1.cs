17a5f8f27cbafd2a6efef7a500f6f428af52d7ec
Ingrid Nataly Garcia Nivia <ingrid.garcia@sinco.com.co>
2025-10-22 15:52:33 -0500

    🟥rojo

---
+++ ConstruirArbolNavidad/ConstruirArbolNavidad/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using AwesomeAssertions;
+
+namespace ConstruirArbolNavidad;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        //arrange
+                var altura = 2;
+                //act
+                var resultado = CreacionArbol(altura);
+                //assert
+                resultado.Should().Be(5);
+    }
+
+    private int CreacionArbol(int altura)
+    {
+        return altura;
+    }
+}
