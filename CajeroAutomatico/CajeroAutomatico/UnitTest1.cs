aa0effc6a3a654fce58fcba9add1892171f3c064
Ingrid Nataly Garcia Nivia <ingrid.garcia@sinco.com.co>
2025-10-23 09:10:00 -0500

    🟥rojo

---
+++ CajeroAutomatico/CajeroAutomatico/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using AwesomeAssertions;
+
+namespace CajeroAutomatico;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        //arrange
+        int saldoRetirar = 5300;
+        //act
+        var resultado = RetiroCajero(saldoRetirar);
+        //assert
+        resultado.Should().Be(false);
+    }
+
+    private object RetiroCajero(int saldoRetirar)
+    {
+        throw new NotImplementedException();
+    }
+}
