3229b6589e1e65244750e6c8ede0860f2d3532ea
Ingrid Nataly Garcia Nivia <ingrid.garcia@sinco.com.co>
2025-10-23 09:27:27 -0500

    🟥rojo

--- CajeroAutomatico/CajeroAutomatico/UnitTest1.cs
+++ CajeroAutomatico/CajeroAutomatico/UnitTest1.cs
@@ -14,12 +14,23 @@ public class UnitTest1
         //assert
         resultado.Should().Be(false);
     }
+    
+    [Fact]
+    public void Saldo_A_Retirar_No_Excede_El_Saldo_Que_Hay_En_El_Cajero()
+    {
+        //arrange
+        int saldoRetirar = 3000;
+        //act
+        var resultado = RetiroCajero(saldoRetirar);
+        //assert
+        resultado.Should().Be(true);
+    }
 
     private object RetiroCajero(int saldoRetirar)
     {
         int saldoCajero = 5100;
         
-        if (saldoRetirar>saldoCajero)
+        if (saldoRetirar!=5100)
         {
             Console.WriteLine("El cajero automático no dispone de dinero suficiente, por favor acuda al cajero automático más cercano");
             return false;
