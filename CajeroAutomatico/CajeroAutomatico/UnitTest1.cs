f02ba541ba63bfc843bdd7f2a8f4b62ff0fb523f
Ingrid Nataly Garcia Nivia <ingrid.garcia@sinco.com.co>
2025-10-24 19:59:10 -0500

    🟦azul

--- CajeroAutomatico/CajeroAutomatico/UnitTest1.cs
+++ CajeroAutomatico/CajeroAutomatico/UnitTest1.cs
@@ -14,12 +14,12 @@ public class UnitTest1
         //assert
         resultado.Should().Be(false);
     }
-    
-    [Fact]
-    public void Saldo_A_Retirar_No_Excede_El_Saldo_Que_Hay_En_El_Cajero()
+
+    [Theory]
+    [InlineData(1700)]
+    [InlineData(900)]
+    public void Saldo_A_Retirar_No_Excede_El_Saldo_Que_Hay_En_El_Cajero(int saldoRetirar)
     {
-        //arrange
-        int saldoRetirar = 3000;
         //act
         var resultado = RetiroCajero(saldoRetirar);
         //assert
@@ -28,14 +28,51 @@ public class UnitTest1
 
     private object RetiroCajero(int saldoRetirar)
     {
-        int saldoCajero = 5100;
+        //Estado inicial del cajero automático
+        int[] valor = { 500, 200, 100, 50, 20, 10, 5, 2, 1 };
+        int[] cantidadDelvalor = { 2, 3, 5, 12, 20, 50, 100, 250, 500 };
         
-        if (saldoRetirar>saldoCajero)
+        //Saldo cajero
+        int saldoCajero = 5100;
+        int valorDescontar = saldoRetirar;
+       
+
+        if (saldoRetirar > saldoCajero)
         {
-            Console.WriteLine("El cajero automático no dispone de dinero suficiente, por favor acuda al cajero automático más cercano");
+            Console.WriteLine(
+                "El cajero automático no dispone de dinero suficiente, por favor acuda al cajero automático más cercano");
             return false;
         }
+
+
+        for (int i = 0; i < cantidadDelvalor.Length; i++)
+        {
+            int cantidadDisponible = cantidadDelvalor[i];
+            if (valorDescontar != 0){
+                for (int y = 0; y < cantidadDisponible; y++)
+                {
+                        if (valorDescontar >= valor[i])
+                        {
+                            valorDescontar = valorDescontar - valor[i];
+                            cantidadDelvalor[i] = cantidadDelvalor[i] - 1;
+                        }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }else{break;}
+        }
+
+        if (valorDescontar == 0)
+        {
+            Console.WriteLine(
+                "El valor a descontar quedo en: " + valorDescontar +
+                ". Se entrego correctamente todo el saldo a retirar");
+        }
+
         return true;
-        
+
     }
+
 }
--- CajeroAutomatico/CajeroAutomatico/obj/Debug/net9.0/CajeroAutomatico.AssemblyInfo.cs
+++ CajeroAutomatico/CajeroAutomatico/obj/Debug/net9.0/CajeroAutomatico.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CajeroAutomatico")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3229b6589e1e65244750e6c8ede0860f2d3532ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54d3c9784d7574e9ffe161bfb89876183f9df851")]
 [assembly: System.Reflection.AssemblyProductAttribute("CajeroAutomatico")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CajeroAutomatico")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
