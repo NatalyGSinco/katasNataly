2160a37d3f51517a4c3196cbdc53d62885b6fde3
Ingrid Nataly Garcia Nivia <ingrid.garcia@sinco.com.co>
2025-10-27 18:57:32 -0500

    🟥rojo

--- Tenis/Tenis/UnitTest1.cs
+++ Tenis/Tenis/UnitTest1.cs
@@ -15,6 +15,18 @@ public class UnitTest1
         //assert
         resultado.Should().Be("El jugador1 tiene el resultado de puntaje: '15' y el jugador2 tiene el resultado de puntaje: '30'");
     }
+    
+    [Fact]
+    public void Si_Los_Dos_Jugadores_Tienen_3_Puntos_Y_Uno_De_Ellos_Un_Punto_Mas_Que_El_Adversario ()
+    {
+        //arrange
+        var puntajeJugador1 = 3;
+        var puntajeJugador2 = 4;
+        //act
+        var resultado = ResultadoPuntaje(puntajeJugador1,puntajeJugador2);
+        //assert
+        resultado.Should().Be("El jugador1 tiene el resultado de puntaje: '40' y el jugador2 tiene el resultado de puntaje: 'ventaja'");
+    }
 
     private object ResultadoPuntaje(int p1, int p2)
     {
