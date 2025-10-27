e6f3c0bebd37b9a8e406e80c17a28fbdbe7299a1
Ingrid Nataly Garcia Nivia <ingrid.garcia@sinco.com.co>
2025-10-27 18:45:48 -0500

    🟥rojo

---
+++ Tenis/Tenis/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+
+namespace Tenis;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Si_Los_Puntos_Son_Del_1_Al_3()
+    {
+        //arrange
+        var puntajeJugador1 = 1;
+        var puntajeJugador2 = 2;
+        //act
+        var resultado = ResultadoPuntaje(puntajeJugador1,puntajeJugador2);
+        //assert
+        resultado.Should().Be("15","30");
+    }
+
+    private object ResultadoPuntaje(int p1, int p2)
+    {
+        throw new NotImplementedException();
+    }
+}
